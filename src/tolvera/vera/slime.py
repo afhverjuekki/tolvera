678695b6ff41cbaa328a0701c8efbd4b652045e8
Jack Armitage <jack.armitage@me.com>
2023-12-01 14:18:08 +0000

    fix state

--- src/tolvera/vera/slime.py
+++ src/tolvera/vera/slime.py
@@ -23,19 +23,19 @@ class Slime:
             'SUBSTEP':     (ti.i32, 1)
         })
         self.tv.s.slime_p = {'state': {
-            'sense_angle':  (ti.math.vec2, 0., 10.),
-            'sense_left':   (ti.math.vec2, 0., 10.),
-            'sense_centre': (ti.math.vec2, 0., 10.),
-            'sense_right':  (ti.math.vec2, 0., 10.),
+            'sense_angle':  (ti.f32, 0., 10.),
+            'sense_left':   (ti.math.vec4, 0., 10.),
+            'sense_centre': (ti.math.vec4, 0., 10.),
+            'sense_right':  (ti.math.vec4, 0., 10.),
         }, 'shape': self.tv.pn,
-        'osc': ('get'), 'randomise': False}
+        'osc': ('get'), 'randomise': True}
         self.tv.s.slime_s = {'state': {
             'sense_angle': (ti.f32, 0., 1.),
             'sense_dist':  (ti.f32, 0., 50.),
             'move_angle':  (ti.f32, 0., 1.),
             'move_dist':   (ti.f32, 0., 4.),
             'evaporate':   (ti.f32, 0., 1.)
-        }, 'shape': (self.tv.sn, self.tv.sn),
+        }, 'shape': self.tv.sn, # multi-species: (self.tv.sn, self.tv.sn),
         'osc': ('set'), 'randomise': True}
         self.trail = Pixels(self.tv, **kwargs)
         self.evaporate = ti.field(dtype=ti.f32, shape=())
@@ -49,8 +49,8 @@ class Slime:
             if field[i].active == 0.0: continue
 
             p    = field[i]
-            ang  = self.tv.s.slime_p[i,0].sense_angle
-            species = self.tv.s.slime_s[p.species, p.species] # diagonal
+            ang  = self.tv.s.slime_p[i].sense_angle
+            species = self.tv.s.slime_s[p.species]
 
             sense_angle = species.sense_angle * self.CONSTS.SENSE_ANGLE
             move_angle  = species.move_angle  * self.CONSTS.MOVE_ANGLE
@@ -70,14 +70,17 @@ class Slime:
             p.pos += ti.Vector([ti.cos(ang), ti.sin(ang)]) \
                 * species.move_dist * p.active
             
-            self.tv.s.slime_p[i,0].sense_angle  = ang
-            self.tv.s.slime_p[i,0].sense_centre = c
-            self.tv.s.slime_p[i,0].sense_left   = l
-            self.tv.s.slime_p[i,0].sense_right  = r
+            self.tv.s.slime_p[i].sense_angle  = ang
+            self.tv.s.slime_p[i].sense_centre = c
+            self.tv.s.slime_p[i].sense_left   = l
+            self.tv.s.slime_p[i].sense_right  = r
             field[i].pos = p.pos
     @ti.func
-    def sense(self, pos, ang, dist):
-        p = pos + ti.Vector([ti.cos(ang), ti.sin(ang)]) * dist
+    def sense(self, pos: ti.math.vec2, ang: ti.f32, dist: ti.f32) -> ti.math.vec4:
+        ang_cos = ti.cos(ang)
+        ang_sin = ti.sin(ang)
+        v = ti.Vector([ang_cos, ang_sin])
+        p = pos + v * dist
         px = ti.cast(p[0], ti.i32) % self.tv.x
         py = ti.cast(p[1], ti.i32) % self.tv.y
         pixel = self.trail.px.rgba[px, py]
@@ -94,16 +97,15 @@ class Slime:
     def deposit_particles(self, particles: ti.template(), species: ti.template()):
         for i in range(particles.shape[0]):
             if particles[i].active == 0.0: continue
-            p, s = particles[i], species[particles[i].species, 0]
+            p, s = particles[i], species[particles[i].species]
             x = ti.cast(p.pos[0], ti.i32) % self.tv.x
             y = ti.cast(p.pos[1], ti.i32) % self.tv.y
-            rgba = ti.Vector([s.r, s.g, s.b, s.a])
-            self.trail.circle(x, y, p.size, rgba * p.active)
-    def step(self, particles):
+            self.trail.circle(x, y, p.size, s.rgba * p.active)
+    def step(self, particles, species):
         for i in range(self.CONSTS.SUBSTEP):
             self.move(particles.field)
-            self.deposit_particles(particles.field, particles.species.field)
+            self.deposit_particles(particles.field, species)
             self.trail.diffuse(self.evaporate[None])
-    def __call__(self, particles):
-        self.step(particles)
+    def __call__(self, particles, species):
+        self.step(particles, species)
         return self.trail.px
