55b070f774f35dfb9e4974eeff3d30b2eb386681
Jack Armitage <jack.armitage@me.com>
2023-12-04 14:34:50 +0000

    better prints

--- src/tolvera/_osc.py
+++ src/tolvera/_osc.py
@@ -14,7 +14,6 @@ class OSC:
         self.init_map(**kwargs)
         print(f"[Tölvera.OSC] OSC initialization complete.")
     def init_osc(self, **kwargs):
-        print(f"[Tölvera.OSC] Initializing OSC...")
         self.host_ip      = kwargs.get('host', "127.0.0.1")
         self.client_ip    = kwargs.get('client', "127.0.0.1")
         self.client_name  = kwargs.get('client_name', self.ctx.name_clean)
@@ -22,16 +21,20 @@ class OSC:
         self.send_port    = kwargs.get('send_port', 5000)
         self.trace        = kwargs.get('osc_trace', False)
         self.verbose      = kwargs.get('osc_verbose', False)
+        print(f"[Tölvera.OSC] Initialising OSC host '{self.host_ip}:{self.receive_port}'.")
         self.host = iiOSC(self.host_ip, self.receive_port, verbose=self.verbose, concurrent=True)
+        print(f"[Tölvera.OSC] Initialising OSC client '{self.client_name}' at '{self.client_ip}:{self.send_port}'.")
         self.host.create_client(self.client_name, self.client_ip, self.send_port)
         if self.trace:
             def trace(address, *args):
                 print(f"[Tölvera.OSC.trace] '{address}' {args}")
             self.host.args('/*')(trace)
     def init_map(self, **kwargs):
-        print(f"[Tölvera.OSC] Initializing OSCMap...")
+        print(f"[Tölvera.OSC] Initialising OSCMap for '{self.client_name}'.")
         self.create_patch   = kwargs.get('create_patch', False)
         self.patch_type     = kwargs.get('patch_type', "Pd")
         self.patch_filepath = kwargs.get('patch_filepath', "tolvera_osc")
         self.export_patch   = kwargs.get('export_patch', None)
+        if self.create_patch:
+            print(f"[Tölvera.OSC] Creating {self.patch_type} patch '{self.patch_filepath}'.")
         self.map = OSCMap(self.host, self.client_name, self.patch_type, self.patch_filepath, self.create_patch, export=self.export_patch)
