ede3d8a88762c727a5de7a9d14f2f542edda2bd8
Jack Armitage <jack.armitage@me.com>
2024-01-17 16:39:19 +0000

    cv2->cv

--- src/tolvera/cv.py
+++ src/tolvera/cv.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import taichi as ti
 
@@ -14,7 +14,7 @@ class CV:
         self.px = Pixels(self.tv, **kwargs)
         self.frame_rgb = np.zeros((self.y, self.x, 3), np.uint8)
         self.ggui_fps_limit = kwargs.get("ggui_fps_limit", 120)
-        self.substeps = kwargs.get("substeps", 1)
+        self.substeps = kwargs.get("substeps", 2)
         self.invert = kwargs.get("invert", True)
         self.colormode = kwargs.get("colormode", "rgba")
         self.device = kwargs.get("device", 0)
@@ -24,10 +24,10 @@ class CV:
 
     def camera_init(self):
         print(f"[{self.tv.name}] Initialising camera device {self.device}...")
-        self.camera_capture = cv2.VideoCapture(self.device)
-        self.camera_x = self.camera_capture.get(cv2.CAP_PROP_FRAME_WIDTH)
-        self.camera_y = self.camera_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        self.camera_fps = self.camera_capture.get(cv2.CAP_PROP_FPS)
+        self.camera_capture = cv.VideoCapture(self.device)
+        self.camera_x = self.camera_capture.get(cv.CAP_PROP_FRAME_WIDTH)
+        self.camera_y = self.camera_capture.get(cv.CAP_PROP_FRAME_HEIGHT)
+        self.camera_fps = self.camera_capture.get(cv.CAP_PROP_FPS)
         self.camera_substeps = (int)(
             self.ggui_fps_limit / self.camera_fps
         ) * self.substeps
@@ -39,11 +39,12 @@ class CV:
     def camera_read(self):
         ret, self.camera_frame = self.camera_capture.read()
         # print(f"[{self.tv.name}] Camera read: {ret}")
+        self.camera_frame = self.camera_frame.astype(np.float32)
         return self.camera_frame
 
     def threshold(self, img, thresh=127, max=255, threshold_type="binary"):
         if threshold_type == "binary":
-            ret, thresh_img = cv2.threshold(img, thresh, max, cv2.THRESH_BINARY)
+            ret, thresh_img = cv.threshold(img, thresh, max, cv.THRESH_BINARY)
         elif threshold_type == "otsu":
             # FIXME: why is this not working?
             """
@@ -53,60 +54,60 @@ class CV:
             >     'scn' is 1
             """
             thresh = 0
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            ret, thresh_img = cv2.threshold(
-                img, thresh, max, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+            img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+            ret, thresh_img = cv.threshold(
+                img, thresh, max, cv.THRESH_BINARY + cv.THRESH_OTSU
             )
         self.thresh_img = thresh_img
         return thresh_img
 
     def find_contours(self, thresh):
-        img = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
-        contours, hierarchy = cv2.findContours(
-            img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+        img = cv.cvtColor(thresh, cv.COLOR_BGR2GRAY)
+        contours, hierarchy = cv.findContours(
+            img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE
         )
         self.contours = contours
         return contours
 
     def approx_poly_dp(self, contours, epsilon=0.1):
-        polygons = [cv2.approxPolyDP(c, epsilon, True) for c in contours]
+        polygons = [cv.approxPolyDP(c, epsilon, True) for c in contours]
         self.polygons = polygons
         return polygons
 
     def draw_contours(self, contours, color=(255, 255, 255), thickness=5):
         img = np.zeros((self.y, self.x), np.uint8)
-        img = cv2.drawContours(img, contours, -1, color, thickness)
+        img = cv.drawContours(img, contours, -1, color, thickness)
         self.contours_img = img
         return img
 
     def gaussian_blur(self, img, ksize=(25, 25), sigmaX=0):
-        img = cv2.GaussianBlur(img, ksize, sigmaX)
+        img = cv.GaussianBlur(img, ksize, sigmaX)
         return img
 
-    def resize(self, img, dsize=(1920, 1080), interpolation=cv2.INTER_LINEAR):
-        img = cv2.resize(img, dsize, interpolation)
+    def resize(self, img, dsize=(1920, 1080), interpolation=cv.INTER_LINEAR):
+        img = cv.resize(img, dsize, interpolation)
         return img
 
     def pyr_down(self, img, factor=1):
         for i in range(factor):
-            img = cv2.pyrDown(img)
+            img = cv.pyrDown(img)
         return img
 
     def pyr_up(self, img, factor=1):
         for i in range(factor):
-            img = cv2.pyrUp(img)
+            img = cv.pyrUp(img)
         return img
 
     def bgr_to_gray(self, img):
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         return img
 
     def gray_to_bgr(self, img):
-        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
         return img
 
     def invert(self, img):
-        img = cv2.bitwise_not(img)
+        img = cv.bitwise_not(img)
         return img
 
     @ti.kernel
@@ -139,13 +140,14 @@ class CV:
         self.i += 1
         if self.i % self.camera_substeps == 0:
             frame = self.camera_read()
-            thresh = self.threshold(frame)
-            contours = self.find_contours(thresh)
-            polygons = self.approx_poly_dp(contours)
+            # thresh = self.threshold(frame)
+            # contours = self.find_contours(thresh)
+            # polygons = self.approx_poly_dp(contours)
             # img      = self.draw_contours(contours)
             # img      = self.gaussian_blur(img)
             # img      = self.resize(img, dsize=(int(1920/4), int(1080/4)))
-            self.cv_to_px(self.camera_frame)
+            # self.cv_to_px(self.camera_frame)
+            self.cv_to_px(frame)
 
     def cleanup(self):
         self.camera_capture.release()
