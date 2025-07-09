8cf4c656f2733a735417a3f2c02a828a65a8a92a
Michael Clemens <michael.clemens@outlook.com>
2025-07-09 07:48:14 -0400

    add kernal repo file

---
+++ generated_kernels/kernels_repository.py
@@ -0,0 +1,11 @@
+"""
+Generated Taichi Kernels Repository
+
+Generated kernels can be reused and referenced by the LLM (TODO)
+"""
+
+import taichi as ti
+import tolvera as tv
+import numpy as np
+from math import pi, sqrt, sin, cos, tan, atan2
+
